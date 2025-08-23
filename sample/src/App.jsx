5a980d50ddc634105fa3ddcb43f31a7a54c97f07
sagaasachin <sagaasachin48@gmail.com>
2025-08-23 12:03:29 +0530

    Add loading spinner component and update App.js

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
-import Card from "./Components/portfolio/PortfolioCard"
-import Header from "./Components/portfolio/PortfolioHeader"
-import Certifi from "./Components/portfolio/PortfolioCertificate"
-import About from "./Components/portfolio/PortfolioAbout"
-import ContactPage from "./Components/portfolio/PortfolioContactPage"
-import Footer from "./Components/portfolio/PortfolioFooter"
-
-
-
+import React, { useState, useEffect } from "react";
+import Header from "./Components/portfolio/PortfolioHeader";
+import About from "./Components/portfolio/PortfolioAbout";
+import Card from "./Components/portfolio/PortfolioCard";
+import Certifi from "./Components/portfolio/PortfolioCertificate";
+import ContactPage from "./Components/portfolio/PortfolioContactPage";
+import Footer from "./Components/portfolio/PortfolioFooter";
+import Loader from "./Components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Simulate loading delay
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <Header />
-      <About />
-      <Card />
-      <Certifi />
-      <ContactPage />
-      <Footer />
-
-      
-
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <About />
+          <Card />
+          <Certifi />
+          <ContactPage />
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
